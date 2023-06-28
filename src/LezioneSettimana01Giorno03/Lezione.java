66e0e7c867e628b4c852faadee3c5b40ffb8584b
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-06-28 10:54:29 +0200

    as

--- src/LezioneSettimana01Giorno03/Lezione.java
+++ src/LezioneSettimana01Giorno03/Lezione.java
@@ -3,8 +3,38 @@ package LezioneSettimana01Giorno03;
 public class Lezione {
 
 	public static void main(String[] args) {
-
-
+		int x = 10, y = 30;
+		boolean test = true;
+		if (test) {
+			System.out.println("Test passato");
+		} else {
+			System.out.println("Test non passato");
+		}
+		System.out.println(x <= y ? "minore" : "maggiore");
+		System.out.println(testNumbers(10, 20, 30));
+		
+		//SWITCH CASE
+		String favoriteColour = "red";
+		switch(favoriteColour) {
+		case "green": {
+			System.out.println("Il colore è verde");
+			break;
+		}
+		case "red": {
+			System.out.println("Il colore è rosso");
+			break;
+		}
+		case "blue": {
+			System.out.println("Il colore è blu");
+			break;
+		}
+		default:
+			System.out.println("Nessuno dei precedenti");
+		}
+		
 	}
 
+	public static int testNumbers(int a, int b, int c) {
+		return a < b ? (a < c ? a : c) : (b > c ? b : c);// Non abusare del ternary operator
+	}
 }
