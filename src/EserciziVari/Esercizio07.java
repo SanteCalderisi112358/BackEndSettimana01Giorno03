fa3088b27d415132cc7e2ffb5a2060e6bae46510
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-07-02 17:25:54 +0200

    Esercizio07

---
+++ src/EserciziVari/Esercizio07.java
@@ -0,0 +1,46 @@
+//Esercizio 1: Somma degli elementi di un array
+//
+//Scrivi un programma che calcoli la somma di tutti gli elementi di un array 
+//di numeri interi.
+
+package EserciziVari;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Esercizio07 {
+
+	public static void main(String[] args) {
+		System.out.println("Quanti numeri vuoi sommare?");
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		int[] arrayNum = new int[num];
+		arrayNum = riempiArray(arrayNum, input);
+		stampaArray(arrayNum);
+		sommaArray(arrayNum);
+		input.close();
+
+	}
+
+	public static int[] riempiArray(int[] array, Scanner input) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.println("Inserisci numero:");
+			array[i] = input.nextInt();
+		}
+		return array;
+	}
+
+	public static void stampaArray(int[] array) {
+		System.out.println("L'array di numeri è:" + Arrays.toString(array));
+
+	}
+
+	public static void sommaArray(int[] array) {
+		int sum = 0;
+		for (int i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		System.out.println("La somma degli elementi dell'array " + Arrays.toString(array) + " è:\n" + sum);
+	}
+
+}
